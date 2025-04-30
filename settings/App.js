0bb5cc19c44aa3c88ea287a5eddec27a049d034b
Farhana Dina <dinafarhana32@gmail.com>
2025-05-01 00:42:12 +0600

    Add files via upload

---
+++ settings/App.js
@@ -0,0 +1,13 @@
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "../register/Register";
+function App() { 
+    return (
+        <>
+            <TopBar />
+            <Settings />
+            <Login />
+            <Register />
+        </>
+    )
+}
